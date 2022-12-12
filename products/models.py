1a590acaca621cb5e0961c1f300759c01868e05b
iama3191 <ire.andreina.z@gmail.com>
2022-12-12 20:21:04 +0000

    Add products app and add it to settings, add categories.json file for the categories

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
