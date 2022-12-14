616388083365dadb36ff7ef531421fa1f983a4ac
iama3191 <ire.andreina.z@gmail.com>
2022-12-14 19:18:02 +0000

    Fix typo on the href of each category on the navbar

--- products/views.py
+++ products/views.py
@@ -14,7 +14,7 @@ def all_products(request):
 
     if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category'].split('_')
+            categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
