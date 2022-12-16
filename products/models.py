fc23ca910e5801afff7dd5d26fee15d9d40cbe36
iama3191 <ire.andreina.z@gmail.com>
2022-12-16 17:28:33 +0000

    Add new characteristics to the product model, and update db. Add new characteristics to the product_detail form

--- products/models.py
+++ products/models.py
@@ -21,6 +21,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_other_flavors = models.BooleanField(default=False, null=True, blank=True)
+    customized_message = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
