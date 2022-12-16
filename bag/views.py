bd3192d912fc10f1427afa79e0e17174de70ef1f
iama3191 <ire.andreina.z@gmail.com>
2022-12-16 18:10:02 +0000

    Add functionality for adding the same product with different flavors

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,25 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    flavor = None
+
+    if 'product_flavor' in request.POST:
+        flavor = request.POST['product_flavor']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if flavor:
+        if item_id in list(bag.keys()):
+            if flavor in bag[item_id]['items_by_flavor'].keys():
+                bag[item_id]['items_by_flavor'][flavor] += quantity
+            else:
+                bag[item_id]['items_by_flavor'][flavor] = quantity
+        else:
+            bag[item_id] = {'items_by_flavor': {flavor: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
