85e73b4f31c37572615a8edbf6f6b5b2b1169349
iama3191 <ire.andreina.z@gmail.com>
2022-12-20 11:29:16 +0000

    Add stripe structure, but needs to fix payment layout: is not showing

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51M5S3gFdieclI0ikyizYAD67IiFj9tLjQaSXEPVTA6fvOjcMkzXF1p383dDKYLojA6wOOovDAG3BkHLQL6GlVXGT00rEoaKhHp',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
