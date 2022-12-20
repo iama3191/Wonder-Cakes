6eaca3eb1c583471190e5341b8d905fa19dffedf
iama3191 <ire.andreina.z@gmail.com>
2022-12-20 11:53:26 +0000

    Add public and secret key as variables, add intent payment to the view

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -28,3 +28,19 @@ var style = {
 
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+            <i class="fa-solid fa-xmark"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
--- wonder_cakes/settings.py
+++ wonder_cakes/settings.py
@@ -170,6 +170,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 70
 STANDARD_DELIVERY_PERCENTAGE =  10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
