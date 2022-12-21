86980f53b52f6d904e70b8aca7b71347cd65aa81
iama3191 <ire.andreina.z@gmail.com>
2022-12-21 22:39:01 +0000

    Modify form to crispy foe the allauth

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 # Create your views here.
 def profile(request):
     """ Display profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
