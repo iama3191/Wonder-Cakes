246dfe316195d1f70513b0fa3bd6b042ba46ca52
iama3191 <ire.andreina.z@gmail.com>
2022-12-25 19:20:39 +0000

    Remove secret key

--- wonder_cakes/settings.py
+++ wonder_cakes/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--vu!s0^x81)#)j=f^(qps%l^zg*^7q^rgp58d=0150vuqs!#6r'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wondercakes.herokuapp.com', 'localhost']
 
