bf77d112e6c8a1ffad22fd4e5763fcf051c029b0
iama3191 <ire.andreina.z@gmail.com>
2022-12-25 12:10:59 +0000

    Create db in elephantsql and create heroku app

--- wonder_cakes/settings.py
+++ wonder_cakes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +123,7 @@ WSGI_APPLICATION = 'wonder_cakes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
