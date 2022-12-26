9323b5be73aa1858034a5945c2d8ab5fe13fb00a
iama3191 <ire.andreina.z@gmail.com>
2022-12-26 00:53:37 +0000

    Modify products model, and change template according this change

--- products/models.py
+++ products/models.py
@@ -21,10 +21,9 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    main_flavor = models.CharField(max_length=40, null=True, blank=True)
     has_other_flavors = models.BooleanField(default=False, null=True, blank=True)
-    customized_message = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
--- wonder_cakes/settings.py
+++ wonder_cakes/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'wonder_cakes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' is os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
