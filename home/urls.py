fddfda22aedba7b2674c631576049c26fc13b48b
iama3191 <ire.andreina.z@gmail.com>
2022-12-27 15:33:42 +0000

    Add about us and FAQ templates, views and urls

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('FAQ/', views.FAQ, name='FAQ'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 
 def index(request):
     """A view to return the index page"""
-    
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """A view to return the about us page"""
+    return render(request, 'home/about_us.html')
+
+
+def FAQ(request):
+    """A view to return the FAQ us page"""
+    return render(request, 'home/FAQ.html')
