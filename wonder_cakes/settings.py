09537361eb12a9553e6230c77beea7bf48c240c9
iama3191 <ire.andreina.z@gmail.com>
2022-12-28 13:44:51 +0000

    Modify style for the navbar, add color like the scheme color and fix favicon link

--- wonder_cakes/settings.py
+++ wonder_cakes/settings.py
@@ -14,6 +14,9 @@ import os
 import dj_database_url
 from pathlib import Path
 
+if os.path.isfile("env.py"):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
