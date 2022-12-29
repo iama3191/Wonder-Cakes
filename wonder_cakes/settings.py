981d31aaa42924483c47232ae602e49513c295fd
iama3191 <ire.andreina.z@gmail.com>
2022-12-29 20:07:50 +0000

    Finish styling the rest of the pages: verification-email and send-email

--- wonder_cakes/settings.py
+++ wonder_cakes/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+
 ALLOWED_HOSTS = ['wondercakes.herokuapp.com', 'localhost']
 
 
